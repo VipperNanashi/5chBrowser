e5b47424a8968d67e09f71fafc4072d43bce8f39
NET MAUI HighSchool <114081915+MAUIHighSchool@users.noreply.github.com>
2023-01-03 18:23:48 +0900

    板一覧取得を頑張った

---
+++ 5chBrowser/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using _5chBrowser.Services;
+using _5chBrowser.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5chBrowser.ViewModels
+{
+    public partial class MainViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<BoardList> boardSource=new ObservableCollection<BoardList>();
+
+        public MainViewModel()
+        {
+            GetBoardList();
+        }
+        private async void GetBoardList()
+        {
+            GetBoardService getBoardService= new GetBoardService();
+            BoardSource = await getBoardService.GetBoard();
+        }
+    }
+}
