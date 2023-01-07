4548a0e6c903cb9622770cb9da4cbcd9addf7a52
NET MAUI HighSchool <114081915+MAUIHighSchool@users.noreply.github.com>
2023-01-07 15:37:12 +0900

    Jsonでの板取得に変更

--- 5chBrowser/Services/GetBoardService.cs
+++ 5chBrowser/Services/GetBoardService.cs
@@ -1,13 +1,11 @@
 ﻿using _5chBrowser.Models;
-using AngleSharp.Html.Dom;
-using AngleSharp.Html.Parser;
-using CommunityToolkit.WinUI.UI.Controls;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace _5chBrowser.Services
@@ -17,62 +15,50 @@ namespace _5chBrowser.Services
         private ObservableCollection<BoardList> TreeSource = new ObservableCollection<BoardList>();
         public async Task<ObservableCollection<BoardList>> GetBoard()
         {
-            string html = await Task.Run(() => GetHTML().Result);
+            var html = await Task.Run(() => GetHTML().Result);
             return HTMLParse(html);
         }
-        private async Task<string> GetHTML()
+        private async Task<BoardInfo> GetHTML()
         {
             HttpClient client = new HttpClient();
             // Call asynchronous network methods in a try/catch block to handle exceptions.
             try
             {
-                using HttpResponseMessage response = await client.GetAsync("https://itest.5ch.net/");
+                using HttpResponseMessage response = await client.GetAsync("https://menu.5ch.net/bbsmenu.json");
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
-                // Above three lines can be replaced with new helper method below
-                // string responseBody = await client.GetStringAsync(uri);
 
-                return responseBody;
+                var col=JsonSerializer.Deserialize<BoardInfo>(responseBody);
+                return col;
             }
-            catch (HttpRequestException ex)
+            catch
             {
-                return ex.Message;
+                return null;
             }
         }
-        private ObservableCollection<BoardList> HTMLParse(string html)
+        private ObservableCollection<BoardList> HTMLParse(BoardInfo html)
         {
-            HtmlParser parser=new HtmlParser();
-            IHtmlDocument doc =parser.ParseDocument(html);
 
-            var Nodes = doc.GetElementById("bbsmenu");
-            var categoryNodes = Nodes.GetElementsByClassName("pure-menu-list");
-            foreach (var n in categoryNodes)
+            foreach (var m in html.menu_list)
             {
-                var categoryName = n.QuerySelector("strong").InnerHtml;
-                var categoriesNode = n.GetElementsByClassName("pure-menu-link-board");
 
                 var childrenList = new ObservableCollection<BoardList>();
-                foreach (var c in categoriesNode)
+                foreach (var c in m.category_content)
                 {
-                    string boardName = c.InnerHtml;
-                    string href = c.GetAttribute("href");
-                    string link = $"https://itest.5ch.net{href}";
+                    childrenList.Add(new BoardList()
                     {
-                        childrenList.Add(new BoardList()
-                        {
-                            BoardTitle = boardName,
-                            BoardURL = link
-                        });
-                    }
-                    
+                        BoardTitle = c.board_name,
+                        BoardURL = c.url
+                    });
                 }
                 TreeSource.Add(new BoardList()
                 {
-                    BoardTitle = categoryName,
+                    BoardTitle = m.category_name,
                     Children = childrenList
                 });
             }
             return TreeSource;
         }
+        
     }
 }
